eb69483190913f6997f234a9a404349a8591a025
ALEJ4NDRO2025 <alejandromejia2007l@gmail.com>
2026-03-16 16:26:36 -0500

    feat: hecho hasta modelo user

---
+++ backend/users/models.py
@@ -0,0 +1,13 @@
+import mongoengine as me
+from datetime import datetime
+
+class User(me.Document):
+    email      = me.EmailField(required=True, unique=True)
+    password   = me.StringField(required=True)
+    first_name = me.StringField(max_length=100)
+    last_name  = me.StringField(max_length=100)
+    is_active  = me.BooleanField(default=True)
+    is_admin   = me.BooleanField(default=False)
+    created_at = me.DateTimeField(default=datetime.utcnow)
+
+    meta = {'collection': 'users'}
