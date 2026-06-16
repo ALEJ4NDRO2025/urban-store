f973e39ddadc8b045810865a422c69f8d9faefc6
TuNombreCorrecto <tu_correo@correcto.com>
2026-06-16 13:29:31 -0500

    Probar correo con backend de consola

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -185,7 +185,7 @@ ssl._create_default_https_context = lambda: ssl_context
 # ═══════════════════════════════════════════════════════════════════════════
 # CONFIGURACIÓN DE CORREO ELECTRÓNICO (SMTP GMAIL) CON CERTIFICADOS FIJOS
 # ═══════════════════════════════════════════════════════════════════════════
-EMAIL_BACKEND = 'config.email_backend.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Para desarrollo, imprime los correos en la consola
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
